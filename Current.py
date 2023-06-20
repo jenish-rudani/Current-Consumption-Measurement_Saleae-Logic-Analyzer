7cbdfad50bd88c034d8eb127f2d3ac95823d4b35
jenish-rudani <jrudani1999@gmail.com>
2023-06-20 11:57:23 -0700

    Initial

---
+++ Current.py
@@ -0,0 +1,35 @@
+from saleae.range_measurements import AnalogMeasurer
+import numpy as np
+from numpy import trapz
+
+class Current(AnalogMeasurer):
+    supported_measurements = ["area"]
+
+    """
+    Class initialisation
+    """
+    def __init__(self, requested_measurements):
+        super().__init__(requested_measurements)
+        self.batches = []
+
+    """
+    Does not process data. Simply attaches
+    samples to be processed later.
+    """
+    def process_data(self, data):
+        self.batches.append(data.samples)
+
+    """
+    Calculates Average Voltage and Divide by 5.6 Ohm Resistor.
+    """  
+    def measure(self):
+        data = np.concatenate(self.batches)
+        averageVoltage = np.mean(data)
+        averageCurrent = averageVoltage / 16.5
+        maxVoltage = np.amax(data)
+        maxCurrent = maxVoltage / 16.5
+        return {"Iavg" : round(averageCurrent,6),
+                "Vavg": round(averageVoltage,6),
+                "Vmax" : round(maxVoltage,6),
+                "Imax" : round(maxCurrent,6)}
+        
---
+++ README.md
@@ -0,0 +1,8 @@
+# AreaUnderCurve
+An extension for Saleae Logic Analyser that determines the area under curve of an analog voltage measurement against 16.5 Ohm resistor to determine average current consumption
+![AUC](Images/AUC.png)
+
+## Instructions
+1. Install this extension by clicking "Install"
+2. Add a measurement by clicking on the "Annotations panel" on the right, then the Measurements "+" icon.
+3. Drag the measurement selection window over your recorded data.
